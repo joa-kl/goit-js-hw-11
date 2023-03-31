3cca692ff1d562b9361f35a4fe1645b607a953b0
Joanna K <joannakleinrm@gmail.com>
2023-03-31 22:05:50 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import Notiflix from 'notiflix';
+
+// const URL = `https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=${customerInput}&image_type=photo&orientation=horizontal&safesearch=true`
+// const URL = `https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=yellow+flower&image_type=photo&orientation=horizontal&safesearch=true`
+
+fetch(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=yellow+flower&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
