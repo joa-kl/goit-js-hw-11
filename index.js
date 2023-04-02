e95d623352d4a56ea833f2b478efb70fc834e6b2
Joanna K <joannakleinrm@gmail.com>
2023-04-02 17:22:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 // import { createCards } from "./src/js/photos";
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // import { fetchArticles } from './src/js/pixabay';
 // import axios from 'axios';
 
@@ -9,26 +11,89 @@ const picturesContainer = document.querySelector(".pictures-container")
 const gallery = document.querySelector('.gallery');
 const searchBtn = document.querySelector(".btn-search");
 
+let query = "";
+let page = 1;
+const perPage = 40;
+
+
+const fetchPictures = async () => {
+    const response = await fetch(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+    const pictures = await response.json();
+    return pictures;
+};
+
+fetchPictures()
+    // .then((response) => {
+    //     console.log(response);
+    //     return response.json();
+    // })
+    .then((data) => renderPicturesList(data))
+    .catch(error => {
+        console.log(error);
+    });
+
+
+searchBtn.addEventListener("click", () => {
+    fetchPictures()
+        .then((pictures) => renderPicturesList(pictures))
+        .catch(() => Notiflix.Notify.failure("Oops, there is no country with that name"))
+    });
+
+
+function onSearch(e) {
+    e.preventDefault();
+    page = 1;
+    query = e.currentTarget.searchQuery.value.trim();
+    clearFormGallery();
+    loadMoreButton.classList.add('is-hidden');
+    if (query === '') {
+        noInfoForSearch();
+        return;
+    }
+
+    fetchPictures()
+        .then(({ data }) => {
+            if (data.totalHits === 0) {
+                alertNoContentFound();
+            } else {
+                createCards(data.hits);
+                const lightbox = new SimpleLightbox('.gallery a', {
+                    captionDelay: 250,
+                }).refresh();
+                addTotalInfoCounter(data);
+
+                if (data.totalHits > perPage) {
+                    loadMoreButton.classList.remove('is-hidden');
+                }
+            }
+        })
+        .catch(error => console.log(error));
+}
+
+// function fetchPictures() {
+//     const URL = `https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+//     return fetch(`${URL}`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(response.status);
+//             }
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
 
 
 
-// TUTAJ SKONCZYŁAM
 
-const query = "flower"
-
-fetch(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`, {
-    method: "GET",
-})
-    .then((response) => {
-        console.log(response);
-        return response.json();
-    })
-    .then((data) => {
-        const hits = Object.values(data.hits);
-        console.log("hits: ", hits);
-        const picturesList = hits
-            .map((hit) => {
-                return `
+function renderPicturesList(data) {
+    const hits = Object.values(data.hits);
+    console.log("hits: ", hits);
+    const picturesList = hits
+        .map((hit) => {
+            return `
                     <li>
                    
                     <div class="photo-card" id=${hit.id}>
@@ -42,17 +107,68 @@ fetch(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&q=${query
                     </div>
                     </a >
                     </li>`;
+            // <li>
+            // <img src="${hit.webformatURL}"></img>
+            // <p>Likes: ${hit.likes}</p>
+            // </li>`;
+        })
+        .join("");
+    gallery.innerHTML = picturesList;
+    // hits.forEach((hit) => {
+    //     console.log(hit.pageURL);
+    // })
+};
+
+
+function addTotalInfoCounter(data) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function noInfoForSearch() {
+    Notiflix.Notify.failure('Please specify your search query.');
+}
+
+function endOfContent() {
+    Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+    );
+}
+
+function alertNoContentFound() {
+    Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+    );
+}
+//     .then((data) => {
+//         const hits = Object.values(data.hits);
+//         console.log("hits: ", hits);
+//         const picturesList = hits
+//             .map((hit) => {
+//                 return `
+//                     <li>
+                   
+//                     <div class="photo-card" id=${hit.id}>
+//                         <img class="gallery__image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+//                         <div class="info">
+//                             <p class="info__item"> <b>Likes</b> ${hit.likes} </p>
+//                             <p class="info__item"> <b>Views</b> ${hit.views} </p>
+//                             <p class="info__item"> <b>Comments</b> ${hit.comments} </p>
+//                             <p class="info__item"> <b>Downloads</b> ${hit.downloads} </p>
+//                         </div>
+//                     </div>
+//                     </a >
+//                     </li>`;
                 // <li>
                 // <img src="${hit.webformatURL}"></img>
                 // <p>Likes: ${hit.likes}</p>
                 // </li>`;
-            })
-            .join("");
-        gallery.innerHTML = picturesList;
+        //     })
+        //     .join("");
+        // gallery.innerHTML = picturesList;
         // hits.forEach((hit) => {
         //     console.log(hit.pageURL);
         // })
-    });
+    // });
 
 
 
