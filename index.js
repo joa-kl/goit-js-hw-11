4934c15b714808e24453d7de9fe2a1d1dfc8af40
Joanna K <joannakleinrm@gmail.com>
2023-04-03 19:10:08 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-// import { createCards } from "./src/js/photos";
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchArticles } from './src/js/pixabay';
+import { renderPicturesList } from "./src/js/photos";
 // import axios from 'axios';
 
-const input = document.querySelector(".input");
-const picturesContainer = document.querySelector(".pictures-container")
+// const input = document.querySelector(".input");
+// const picturesContainer = document.querySelector(".pictures-container")
 const gallery = document.querySelector('.gallery');
 const searchBtn = document.querySelector(".btn-search");
 const searchForm = document.querySelector('.search-form');
@@ -63,9 +62,9 @@ function onSearch(e) {
             console.log(alertNoContentFound());
             } else {
                 renderPicturesList(data);
-                // const lightbox = new SimpleLightbox('.gallery a', {
-                //     captionDelay: 250,
-                // }).refresh();
+                const lightbox = new SimpleLightbox('.gallery a', {
+                    captionDelay: 250,
+                }).refresh();
                 addTotalInfoCounter(data);
 
                 if (data.totalHits > perPage) {
@@ -77,33 +76,34 @@ function onSearch(e) {
 }
 
 
-function renderPicturesList(data) {
-    console.log(data.totalHits);
-    const hits = Object.values(data.hits);
-    console.log("hits: ", hits);
-    const picturesList = hits
-        .map((hit) => {
-            return `
-                <li class="gallery__item">
-                    <div class="photo-card" id=${hit.id}>
-                        <a  href="${hit.largeImageURL}" onclick="return false;">
-                            <img class="gallery__image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
-                            <div class="info">
-                                <p class="info__item"> <b>Likes</b> ${hit.likes} </p>
-                                <p class="info__item"> <b>Views</b> ${hit.views} </p>
-                                <p class="info__item"> <b>Comments</b> ${hit.comments} </p>
-                                <p class="info__item"> <b>Downloads</b> ${hit.downloads} </p>
-                            </div>
-                   
-                        </a>
-                     </div>
-                </li>`;
-          
-        })
-        .join("");
-    gallery.innerHTML = picturesList;
-  
-};
+
+
+
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}">`,
+
+        {
+            onShow: instance => {
+                window.addEventListener('keydown', closeModal);
+            }
+        }
+    );
+    function closeModal(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    }
+
+    instance.show();
+}
+
 
 
 function onLoadMore() {
--- src/js/photos.js
+++ src/js/photos.js
@@ -1,33 +1,30 @@
-export { createCards };
+export { renderPicturesList };
 const gallery = document.querySelector('.gallery');
-function createCards(hits) {
-    const cards = hits
-        .map(hit => {
-            const {
-                id,
-                largeImageURL,
-                webformatURL,
-                tags,
-                likes,
-                views,
-                comments,
-                downloads,
-            } = hit;
-            return `
-        <a class="gallery__item" href="${largeImageURL}" onclick="return false;">
-        <div class="photo-card" id=${id}>
-        <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-          <p class="info__item"> <b>Likes</b> ${likes} </p>
-          <p class="info__item"> <b>Views</b> ${views} </p>
-          <p class="info__item"> <b>Comments</b> ${comments} </p>
-          <p class="info__item"> <b>Downloads</b> ${downloads} </p>
-        </div>
-      </div>
-      </a>`;
-        })
-        .join('');
 
-    gallery.insertAdjacentHTML('beforeend', cards);
-}
+function renderPicturesList(data) {
+  console.log(data.totalHits);
+  const hits = Object.values(data.hits);
+  console.log("hits: ", hits);
+  const picturesList = hits
+    .map((hit) => {
+      return `
+                <li class="gallery__item">
+                    <div class="photo-card" id=${hit.id}>
+                        <a  href="${hit.largeImageURL}" onclick="return false;">
+                            <img class="gallery__image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+                            <div class="info">
+                                <p class="info__item"> <b>Likes</b> ${hit.likes} </p>
+                                <p class="info__item"> <b>Views</b> ${hit.views} </p>
+                                <p class="info__item"> <b>Comments</b> ${hit.comments} </p>
+                                <p class="info__item"> <b>Downloads</b> ${hit.downloads} </p>
+                            </div>
+                   
+                        </a>
+                     </div>
+                </li>`;
 
+    })
+    .join("");
+  gallery.innerHTML = picturesList;
+
+};
